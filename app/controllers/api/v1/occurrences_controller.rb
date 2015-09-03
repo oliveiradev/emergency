afbc3a7a550e668c931f9ca3c4b3b3b9329c8531
oliveiradev <felipe.oliveiradev@gmail.com>
2015-09-03 17:05:26 -0300

    ...

--- app/controllers/api/v1/occurrences_controller.rb
+++ app/controllers/api/v1/occurrences_controller.rb
@@ -2,11 +2,18 @@ class Api::V1::OccurrencesController < ApplicationController
   before_action :set_occurrence , only: [:show,:update,:destroy]
 
   def index
-    @occurrneces = Occurrence.all
-    render json: @occurrneces
+    @occurrences = Occurrence.all
+    render json: @occurrences
   end
 
   def show
+    render json: @occurence
+  end
+
+  def show_geojson
+    @occurrences = Occurrence.where(pending: 0)
+
+    render json: prepare_geojson(@occurrences)
   end
 
   def create
@@ -29,6 +36,25 @@ class Api::V1::OccurrencesController < ApplicationController
 
   def set_occurrence
     @occurence = Occurrence.find(params[:id])
+  end
+
+  def prepare_geojson(occurrences)
+    array_json = Array.new
+    occurrences.each do |occurence|
+      json_response = {}
+      json_response["type"] = "Feature"
+      json_response["geometry"] =  {
+          type: "Point",
+          coordinates: [occurence.lng , occurence.lat]
+      }
+      json_response["properties"] = {
+          'title': occurence.description,
+          'marker-color': '#FF0000',
+          'marker-symbol': 'hospital'
+      }
+      array_json.push(json_response)
+    end
+    array_json
   end
 
   def set_params
